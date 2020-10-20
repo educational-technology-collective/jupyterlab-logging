b288af481d044e8de4698d7f499761d53f20136e
Damian Avila <damianavila82@yahoo.com.ar>
2020-10-20 13:28:47 -0300

    Add conda packaging. Workaround aws-kinesis-writable unavailability adding files to the final package.

---
+++ conda.recipe/post-link.sh
@@ -0,0 +1,3 @@
+tar -xvzf ${CONDA_PREFIX}/share/jupyter/lab/extensions/jupyterlab-logging-0.1.0.tgz -C ${CONDA_PREFIX}/share/jupyter/lab/extensions/
+${CONDA_PREFIX}/bin/jupyter labextension link $CONDA_PREFIX/share/jupyter/lab/extensions/package/aws-kinesis-writable
+${CONDA_PREFIX}/bin/jupyter lab build
